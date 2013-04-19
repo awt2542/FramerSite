f66503b8594208cb655a2b078454240d581b3c68
Koen Bok <koen@fb.com>
2013-04-19 13:32:10 -0700

    Add pages, made menubar work

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
-	console.log("Hello!");
+NavigationMap = {
+	"": 			"about",
+	"learn": 		"learn",
+	"examples": 	"examples",
+	"docs": 		"docs",
+	"community": 	"community",
+}
+
+$(function(){
+	
+	// Set the body class to the sub page
+	
+	var className = "about";
+	var parts = window.location.pathname.split("/");
+	
+	if (parts.length > 2) {
+		if (NavigationMap.hasOwnProperty(parts[1])) {
+			className = NavigationMap[parts[1]];
+		};
+	};
+		
+	$("body").addClass(className);
+	
+	// Select the menu link
+	
+	$("#global-nav li." + className).addClass("selected");
+	
 });
