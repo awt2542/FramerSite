bc38d7c4f12e1d49e9d441ea5501bda3331b78dc
Koen Bok <koen@fb.com>
2013-05-04 14:24:18 -0700

    Add retina script

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,43 @@
+// To make images retina, add a class "2x" to the img element
+// and add a <image-name>@2x.png image.
+
+function isRetina() {
+	
+	// return true // You know, for testing!
+	
+	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+					  (min--moz-device-pixel-ratio: 1.5),\
+					  (-o-min-device-pixel-ratio: 3/2),\
+					  (min-resolution: 1.5dppx)";
+
+	if (window.devicePixelRatio > 1)
+		return true;
+
+	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+		return true;
+
+	return false;
+};
+
+
+function retina() {
+	
+	if (!isRetina()) {
+		return;
+	}
+	
+	$("img.2x").map(function(i, image) {
+		
+		var path = $(image).attr("src");
+		
+		path = path.replace(".png", "@2x.png");
+		path = path.replace(".jpg", "@2x.jpg");
+		
+		$(image).attr("src", path);
+	});
+};
+
+
 function startFramerDemo() {
 	$("#screen").attr("src", "/static/examples/Intro/index.html");
 }
@@ -7,6 +47,7 @@ $(document).ready(function() {
 	// Only start loading the dramer demo after a second
 	setTimeout(startFramerDemo, 1000);
 	
+	retina();
 	
 	// // Manage the height of the more block
 	// function setMoreHeight() {
