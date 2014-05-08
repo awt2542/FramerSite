1175bc4250c142340456ab0af782d9862244143f
benjaminnathan <benjamin@benjaminnathan.nl>
2014-05-08 10:40:48 +0200

    really close

--- static/js/scroller.coffee
+++ static/js/scroller.coffee
@@ -1,9 +1,6 @@
 $(document).ready ->
-	updateActive = ->
-		# offset = window.innerHeight / 4
+	updateActive = (doScroll=true) ->
 		offset = 60
-		
-		# anchors = $("a").map -> $(this)
 		fromTop = $(window).scrollTop();
 		activeAnchor = null
 		
@@ -13,22 +10,25 @@ $(document).ready ->
 			if anchor.offset().top < fromTop + offset
 				activeAnchor = anchor
 		
+				
 		if activeAnchor			
 			className = activeAnchor.attr "name"
 			$("#sidebar a").removeClass "active"
 			$("#sidebar a.#{className}").addClass "active"	
 			$("#sidebar a.#{className}").parent().parent().addClass "appear"
-			$("#sidebar").scrollTop($("#sidebar a.#{className}").scrollTop());
+			$("#sidebar a.#{className}").parent().parent().parent().parent().parent().addClass "has-active"
 			
+			if doScroll
+				
+				$("#sidebar.has-active").scrollTop($("#sidebar a.#{className}").scrollTop() + 80);			
 			
-	$(window).scroll updateActive
-	$(window).resize updateActive
-	$(window).load updateActive
+	$(window).scroll -> updateActive false
+	$(window).resize -> updateActive false
+	$(window).load -> updateActive true
+	
 	
 	$("#sidebar a").click ->
 		$("#sidebar a").removeClass "active"
 		$(this).addClass "active"
 
 
-
-		
--- static/js/scroller.js
+++ static/js/scroller.js
@@ -1,8 +1,11 @@
 (function() {
   $(document).ready(function() {
     var updateActive;
-    updateActive = function() {
+    updateActive = function(doScroll) {
       var activeAnchor, className, fromTop, offset;
+      if (doScroll == null) {
+        doScroll = true;
+      }
       offset = 60;
       fromTop = $(window).scrollTop();
       activeAnchor = null;
@@ -18,12 +21,21 @@
         $("#sidebar a").removeClass("active");
         $("#sidebar a." + className).addClass("active");
         $("#sidebar a." + className).parent().parent().addClass("appear");
-        return $("#sidebar").scrollTop($("#sidebar a." + className).scrollTop());
+        $("#sidebar a." + className).parent().parent().parent().parent().parent().addClass("has-active");
+        if (doScroll) {
+          return $("#sidebar.has-active").scrollTop($("#sidebar a." + className).scrollTop() + 80);
+        }
       }
     };
-    $(window).scroll(updateActive);
-    $(window).resize(updateActive);
-    $(window).load(updateActive);
+    $(window).scroll(function() {
+      return updateActive(false);
+    });
+    $(window).resize(function() {
+      return updateActive(false);
+    });
+    $(window).load(function() {
+      return updateActive(true);
+    });
     return $("#sidebar a").click(function() {
       $("#sidebar a").removeClass("active");
       return $(this).addClass("active");
