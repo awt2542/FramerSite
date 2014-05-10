dbafa193bccc0b9bd145a80501d341f6b2beae9e
benjaminnathan <benjamin@benjaminnathan.nl>
2014-05-10 15:58:01 +0200

    Example block

--- static/examples/codeblock/app.js
+++ static/examples/codeblock/app.js
@@ -1,15 +1,16 @@
 // Define the imageLayer
 imageLayer = new Layer({x:0, y:0, width:115, height:121, image:"images/icon.png"})
 imageLayer.center()
+imageLayer.y = 50
 
 // Define a set of states with names (the original state is 'default')
 imageLayer.states.add({
-	bounce: {scale:0.9},
+	bounce: {scale:0.85},
 })
 
 // Set the default animation options
 imageLayer.states.animationOptions = {
-	curve: "spring(500,20,100)"
+	curve: "spring(500,30,100)"
 }
 
 // On a click, go to the next state
