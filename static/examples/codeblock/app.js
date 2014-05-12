add5613e9b3570470c3120a17a62cf43c089ecaa
benjaminnathan <benjamin@benjaminnathan.nl>
2014-05-12 15:50:30 +0200

    Index special case fix

--- static/examples/codeblock/app.js
+++ static/examples/codeblock/app.js
@@ -1,25 +1,25 @@
 // Define the imageLayer
 imageLayer = new Layer({x:0, y:0, width:115, height:121, image:"images/icon.png"})
 imageLayer.center()
-imageLayer.y = 50
+imageLayer.y = 38
 
 window.onresize = function() {
 	imageLayer.center()
-	imageLayer.y = 50	
+	imageLayer.y = 38	
 	imageLayer.x = parseInt(imageLayer.x)
 	
 }
 
 // Define a set of states with names (the original state is 'default')
 imageLayer.states.add({
-	bounce: {scale: 0.8},
-	hue: {hueRotate: -20},
-	rotate: {rotationZ: 90},
+	bounce: {scale: 0.6},
+	hue: {hueRotate: -30, scale:1 },
+	rotate: {rotationZ: 90, blur: 0, scale:1},
 })
 
 // Set the default animation options
 imageLayer.states.animationOptions = {
-	curve: "spring(600,18,0)"
+	curve: "spring(600,16,0)"
 }
 
 // On a click, go to the next state
